e4d5dd3a4f9bebbe87f8e2a7e95631922bc80b1f
MuthoniGathu <gathupenninah@gmail.com>
2019-05-31 12:01:27 +0300

    adding starting project

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '7aa36dd6a67740f8b294f66193434904'
+});
+
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id','=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries =>{
+  	res.json(entries[0]);
+  })
+  .catch(err => res.status(400).json('Unable to get entries'))
+}
+
+module.exports = {
+	handleImage: handleImage ,
+	handleApiCall: handleApiCall
+}
